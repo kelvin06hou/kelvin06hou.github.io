f0b9941391aba774cc3f282b4a49ba15cad72931
kelvin06hou <kelvinho0328@gmail.com>
2025-11-24 06:14:25 +0000

    1

--- alert.js
+++ alert.js
@@ -1,9 +1,3 @@
-alert("This is an external JS file!");
-function saveData() {
-    const value = document.getElementById("userInput").value;
-    localStorage.setItem("myData", value);
-}
-
 window.onload = function() {
     const saved = localStorage.getItem("myData");
     if (saved) document.getElementById("result").textContent = saved;
