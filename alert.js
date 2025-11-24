19604b8bc19aaa7bd856a11454605fecfffb255d
kelvin06hou <kelvinho0328@gmail.com>
2025-11-24 06:22:46 +0000

    1

--- alert.js
+++ alert.js
@@ -3,12 +3,12 @@ window.onload = function() {
     if (saved) document.getElementById("result").textContent = saved;
 };
 
-        document.getElementById('contactForm').addEventListener('submit', function(event) {
-          event.preventDefault();
-          const name = document.getElementById('name').value;
-          const email = document.getElementById('email').value;
-          localStorage.setItem('userName', name);
-          localStorage.setItem('userEmail', email);
-          alert('Data saved locally!');
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    localStorage.setItem('userName', name);
+    localStorage.setItem('userEmail', email);
+    alert('Data saved locally!');
           // Optionally, redirect or clear the form
-        });
+    });
